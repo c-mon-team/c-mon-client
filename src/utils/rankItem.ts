2b91228d660035ae58ef5e3fc22290ca2d6be718
Sohee KWON <soheekwon@naver.com>
2022-11-27 10:21:38 +0900

    feat: add rankItem

---
+++ src/utils/rankItem.ts
@@ -0,0 +1,16 @@
+const rankItem = [
+  { title: '여행', url: 'assets/icons/ic_travel.svg' },
+  { title: '영화/드라마', url: 'assets/icons/ic_movie.svg' },
+  { title: '반려 동물', url: 'assets/icons/ic_pet.svg' },
+  { title: '스포츠', url: 'assets/icons/ic_sports.svg' },
+  { title: '건강 운동', url: 'assets/icons/ic_health.svg' },
+  { title: '책', url: 'assets/icons/ic_book.svg' },
+  { title: '음악', url: 'assets/icons/ic_music.svg' },
+  { title: '음식', url: 'assets/icons/ic_food.svg' },
+  { title: '취미', url: 'assets/icons/ic_hobby.svg' },
+  { title: '패션/뷰티', url: 'assets/icons/ic_beauty.svg' },
+  { title: '대학생활', url: 'assets/icons/ic_univ.svg' },
+  { title: 'IT/재태크', url: 'assets/icons/ic_it.svg' },
+];
+
+export default rankItem;
