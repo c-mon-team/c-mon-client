64d3f430f3f76ad828430fee6ee6ea677e773e89
Sohee KWON <soheekwon@naver.com>
2022-12-04 14:45:21 +0900

    Kako 공유 API 연결

--- src/components/result/Invite.tsx
+++ src/components/result/Invite.tsx
@@ -1,7 +1,59 @@
 import { message } from 'antd';
-import React from 'react';
+import React, { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
 
 function Invite() {
+  useEffect(() => {
+    if (window.Kakao) {
+      const kakao = window.Kakao;
+      initKakao(kakao);
+      createKakaoShareBtn();
+    }
+  }, []);
+
+  const initKakao = (kakao: any) => {
+    if (!kakao.isInitialized()) {
+      kakao.init(process.env.KAKAO_API_KEY);
+    }
+  };
+
+  const createKakaoShareBtn = () => {
+    if (window.Kakao) {
+      window.Kakao.Link.createDefaultButton({
+        objectType: 'feed',
+        container: '#kakao-share-btn',
+        content: {
+          title: '커몬 :: 우리 모임 공통 관심사 테스트',
+          description: '겹치는 관심사를 알려줘 서로를 더 잘 알아갈 수 있어요!',
+          imageUrl: 'https://ifh.cc/g/XG37ZY.png',
+          link: {
+            mobileWebUrl: 'https://c-mon.xyz',
+            webUrl: 'https://c-mon.xyz',
+          },
+        },
+        social: {
+          likeCount: 999,
+          commentCount: 999,
+          sharedCount: 999,
+        },
+        buttons: [
+          {
+            title: '웹으로 보기',
+            link: {
+              mobileWebUrl: window.location.href,
+              webUrl: window.location.href,
+            },
+          },
+        ],
+      });
+    }
+  };
+
   const copyLink = async () => {
     try {
       await navigator.clipboard.writeText(
@@ -17,7 +69,10 @@ function Invite() {
     <div className="py-40 px-20">
       <h2 className="text-title2 mb-18">이 모임에 친구 초대하기</h2>
       <div className="flex gap-15">
-        <button className="flex-1 flex justify-center items-center text-body2 text-gray10 h-80 bg-gray3 rounded-20">
+        <button
+          id="kakao-share-btn"
+          className="flex-1 flex justify-center items-center text-body2 text-gray10 h-80 bg-gray3 rounded-20"
+        >
           <img className="mr-10" src="/assets/icons/ic_kakao.svg" alt="kakao" />
           카카오톡
           <br />
